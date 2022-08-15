fef97d93721e338d4296c73b45d5501eded8acca
Zakaria Fikri <fikrizakaria@outlook.com>
2022-08-15 09:38:41 +0100

    pushing after successful launch on mobile

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, {Children, cloneElement} from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 const Stack = createNativeStackNavigator();
 export function Navigator({children}) {
-  return <Stack.Navigator></Stack.Navigator>;
+  return (
+    <Stack.Navigator>
+      {Children.map(Children.toArray(children), (Elt, i) => (
+        <Stack.Screen name={i} component={cloneElement(Elt)}></Stack.Screen>
+      ))}
+    </Stack.Navigator>
+  );
 }
 export function Screen({name, component}) {
   return <Stack.Screen name={name} component={component}></Stack.Screen>;
