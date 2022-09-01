47e5cf106b9d95b61b19a8319263a49b80ae7451
Zakaria Fikri <fikrizakaria@outlook.com>
2022-09-01 17:05:13 +0100

    fixing navigation problem

--- components/Navigation.js
+++ components/Navigation.js
@@ -2,14 +2,6 @@ import React, {Children, cloneElement} from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 const Stack = createNativeStackNavigator();
 export function Navigator({children}) {
-  return (
-    <Stack.Navigator>
-      {Children.map(Children.toArray(children), (Elt, i) => (
-        <Stack.Screen name={i} component={cloneElement(Elt)}></Stack.Screen>
-      ))}
-    </Stack.Navigator>
-  );
+  return <Stack.Navigator>{children}</Stack.Navigator>;
 }
-export function Screen({name, component}) {
-  return <Stack.Screen name={name} component={component}></Stack.Screen>;
-}
+export const Screen = Stack.Screen;
